2eb0257900041c35b3dfc8913cff8173945f1737
lzd <mangoxiaoda@gmail.com>
2020-11-26 08:55:47 +0000

    众筹商品管理

--- database/admin.sql
+++ database/admin.sql
@@ -21,7 +21,7 @@
 
 LOCK TABLES `admin_menu` WRITE;
 /*!40000 ALTER TABLE `admin_menu` DISABLE KEYS */;
-INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,NULL,'2019-10-01 01:55:55'),(2,0,7,'系统管理','fa-tasks',NULL,NULL,NULL,'2020-10-11 02:03:23'),(3,2,8,'管理员','fa-users','auth/users',NULL,NULL,'2020-10-11 02:03:23'),(4,2,9,'角色','fa-user','auth/roles',NULL,NULL,'2020-10-11 02:03:23'),(5,2,10,'权限','fa-ban','auth/permissions',NULL,NULL,'2020-10-11 02:03:23'),(6,2,11,'菜单','fa-bars','auth/menu',NULL,NULL,'2020-10-11 02:03:23'),(7,2,12,'操作日志','fa-history','auth/logs',NULL,NULL,'2020-10-11 02:03:23'),(8,0,2,'用户管理','fa-users','/users',NULL,'2019-10-01 02:02:46','2019-10-01 02:02:50'),(9,0,4,'商品管理','fa-cubes','/products',NULL,'2019-10-01 02:14:16','2020-10-11 02:03:23'),(10,0,5,'订单管理','fa-rmb','/orders',NULL,'2019-10-05 08:59:12','2020-10-11 02:03:23'),(11,0,6,'优惠券管理','fa-tags','/coupon_codes',NULL,'2019-10-05 09:37:43','2020-10-11 02:03:23'),(12,0,3,'类目管理','fa-bars','/categories',NULL,'2020-10-11 02:03:01','2020-10-11 02:03:23');
+INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,NULL,'2019-10-01 01:55:55'),(2,0,9,'系统管理','fa-tasks',NULL,NULL,NULL,'2020-11-26 08:33:01'),(3,2,10,'管理员','fa-users','auth/users',NULL,NULL,'2020-11-26 08:33:01'),(4,2,11,'角色','fa-user','auth/roles',NULL,NULL,'2020-11-26 08:33:01'),(5,2,12,'权限','fa-ban','auth/permissions',NULL,NULL,'2020-11-26 08:33:01'),(6,2,13,'菜单','fa-bars','auth/menu',NULL,NULL,'2020-11-26 08:33:01'),(7,2,14,'操作日志','fa-history','auth/logs',NULL,NULL,'2020-11-26 08:33:01'),(8,0,2,'用户管理','fa-users','/users',NULL,'2019-10-01 02:02:46','2019-10-01 02:02:50'),(9,0,4,'商品管理','fa-cubes','/products',NULL,'2019-10-01 02:14:16','2020-10-11 02:03:23'),(10,0,7,'订单管理','fa-rmb','/orders',NULL,'2019-10-05 08:59:12','2020-11-26 08:33:01'),(11,0,8,'优惠券管理','fa-tags','/coupon_codes',NULL,'2019-10-05 09:37:43','2020-11-26 08:33:01'),(12,0,3,'类目管理','fa-bars','/categories',NULL,'2020-10-11 02:03:01','2020-10-11 02:03:23'),(13,9,6,'众筹商品','fa-flag-checkered','/crowdfunding_products',NULL,'2020-11-26 08:31:21','2020-11-26 08:33:15'),(14,9,5,'普通商品','fa-cubes','/products',NULL,'2020-11-26 08:32:39','2020-11-26 08:33:15');
 /*!40000 ALTER TABLE `admin_menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-24  6:22:00
+-- Dump completed on 2020-11-26  8:55:27
